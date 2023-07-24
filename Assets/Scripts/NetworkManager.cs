a0a57d410cc3900db13d6d5a99440f9d99073452
nandaotop <83474890+nandaotop@users.noreply.github.com>
2023-07-24 18:49:46 -0300

    Initial commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public static NetworkManager instance;
+    [SerializeField]
+    GameObject connectPanel = null;
+
+    private void Awake() {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        connectPanel.SetActive(false);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected");
+    }
+}
